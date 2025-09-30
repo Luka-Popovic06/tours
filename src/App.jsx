6774571b6123f93c7ad72e56708e6bd62b4e31a2
Luka Popovic <lukapopovic0111@gmail.com>
2025-09-30 22:12:33 +0200

    Implement tour list rendering with delete functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,23 @@
+import { useState } from "react";
+import "./App.css";
+import Tour from "./Tour";
+import { info } from "./info.js";
+function App() {
+  const [tours, setTours] = useState(info);
+  const deleteTour = (id) => {
+    setTours((prev) => {
+      return prev.filter((tour) => tour.id !== id);
+    });
+  };
+  return (
+    <>
+      <ul className="list">
+        {tours.map((tour) => {
+          return <Tour key={tour.id} {...tour} onDelete={deleteTour} />;
+        })}
+      </ul>
+    </>
+  );
+}
 
+export default App;
